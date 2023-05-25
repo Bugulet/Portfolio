d079b1b85c40c4bda17a62e28eb8691ebe7caacd
Bug <eusebiu.buga@gmail.com>
2023-05-25 22:15:02 +0300

    playing with shadows

--- src/components/ProjectCardComponent.js
+++ src/components/ProjectCardComponent.js
@@ -6,7 +6,9 @@ import Image from "next/image";
 export default function ProjectCardComponent() {
 
     return (
-        <div className="surface-0 p-4 shadow-2 rounded-lg rounded-tl-3xl rounded-br-3xl border-2 bg-black xl:skew-x-3 hover:lg:scale-105 hover:scale-95 hover:z-50 transition duration-300 ">
+        <div className=" p-4 shadow-2 rounded-lg rounded-tl-3xl rounded-br-3xl border-2 bg-black shadow-sm shadow-cyan-600 
+        hover:shadow-2xl
+        xl:skew-x-1 hover:lg:skew-x-3 hover:scale-95 hover:z-50 transition duration-300 ">
 
             <div className="mb-5 flex flex-col">
                 <img src={"https://picsum.photos/500/500"} className="  rounded-lg rounded-tl-3xl rounded-br-3xl w-full object-fill min-h-0 max-h-96 "></img>
