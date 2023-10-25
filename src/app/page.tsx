62a6b9f33fc76ff3362d70474ca9c090715b1ad0
Bug <eusebiu.buga@gmail.com>
2023-10-25 23:28:56 +0300

    bootstrap and routing added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Container from 'react-bootstrap/esm/Container';
 import Col from 'react-bootstrap/esm/Col';
 import Row from 'react-bootstrap/esm/Row';
 import { Nav } from 'react-bootstrap';
+import Link from 'next/dist/client/link';
 
 
 export default function Home() {
@@ -13,16 +14,15 @@ export default function Home() {
     <>
       <Container fluid className={styles.container}>
         <Row>
-
-          <h1 className="h-100 d-flex align-items-center justify-content-center ">Bugulet</h1>
+          <h1 className=" h-100 d-flex align-items-center justify-content-center align-middle">Hi, I'm Bugulet</h1>
           <p className="d-flex align-items-center justify-content-center m-0">Buga Eusebiu Mihail</p>
           <p className="d-flex align-items-center justify-content-center ">Programmer of all kinds</p>
         </Row>
         <Row>
           <Nav className="h-100 p-3 d-flex align-items-center justify-content-center">
-            <Button className="m-1">Projects</Button>
-            <Button className="m-1">Blog</Button>
-            <Button className="m-1">Contact</Button>
+            <Button className="m-1" href="/projects">Projects </Button>
+            <Button className="m-1" href="/blog">Blog</Button>
+            <Button className="m-1" href="/contacs">Contact</Button>
           </Nav>
         </Row>
       </Container>
